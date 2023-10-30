59cf861848a6aaaa82072a07b7b5252f3d42ab7c
leeyuan <dbdks32123@gmail.com>
2023-10-30 15:56:09 +0900

    GET 요청 시 index 응답

--- app.js
+++ app.js
@@ -7,3 +7,8 @@ const port = 8080; // 사용할 포트 번호
 
 // 정적 파일을 제공하기 위한 미들웨어 설정
 app.use(express.static('public'));
+
+// 루트 경로로 접근시 index.html 파일 전송
+app.get('/', (req, res) => {
+  res.sendFile(__dirname + '/public/index.html');
+});
